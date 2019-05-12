be31e98bf53c64fd3982185f23ef7c4afcb7be8a
TarekMostafa <tarek.mostafa.105@gmail.com>
2019-05-12 16:52:55 +0200

    add encrypt method to aesCBC class

--- server/utilities/aesCBC.js
+++ server/utilities/aesCBC.js
@@ -2,16 +2,28 @@ const aesjs = require('aes-js');
 
 class AesCBC {
   static decrypt(hexPassword) {
-    const key = 'versicherungsausweisdeutschland1';
-    const iv = 'sehenswurdigkeit';
-    const keyBytes = aesjs.utils.utf8.toBytes(key);
-    const ivBytes = aesjs.utils.utf8.toBytes(iv);
+    const keyBytes = aesjs.utils.utf8.toBytes(AesCBC.key);
+    const ivBytes = aesjs.utils.utf8.toBytes(AesCBC.iv);
     // Decrypt Password
     const aesCbc = new aesjs.ModeOfOperation.cbc(keyBytes, ivBytes);
     let decryptedBytes = aesCbc.decrypt(aesjs.utils.hex.toBytes(hexPassword));
     decryptedBytes = aesjs.padding.pkcs7.strip(decryptedBytes);
     return aesjs.utils.utf8.fromBytes(decryptedBytes);
   }
+
+  static encrypt(plainText) {
+    const keyBytes = aesjs.utils.utf8.toBytes(AesCBC.key);
+    const ivBytes = aesjs.utils.utf8.toBytes(AesCBC.iv);
+    // Encrypt Password
+    const aesCbc = new aesjs.ModeOfOperation.cbc(keyBytes, ivBytes);
+    let textBytes = aesjs.utils.utf8.toBytes(plainText);
+    textBytes = aesjs.padding.pkcs7.pad(textBytes);
+    const encryptedBytes = aesCbc.encrypt(textBytes);
+    return aesjs.utils.hex.fromBytes(encryptedBytes);
+  }
 }
+
+AesCBC.key = 'versicherungsausweisdeutschland1';
+AesCBC.iv = 'sehenswurdigkeit';
 
 module.exports = AesCBC;
